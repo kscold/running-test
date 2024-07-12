f637c0129268ce1a63584123c67d29c6d9f56610
kscold <chan6502@gmail.com>
2024-07-13 04:38:25 +0900

    계산 함수 부분 주석 설명 업데이트
    계산 함수 부분 주석 설명 업데이트

--- src/redux/location.js
+++ src/redux/location.js
@@ -1,38 +1,17 @@
-// import { createSlice } from "@reduxjs/toolkit"
-
-// const locationSlice = createSlice({
-//   name: "location",
-//   initialState: {
-//     latitude: null,
-//     longitude: null,
-//     path: [],
-//   },
-//   reducers: {
-//     setLocation: (state, action) => {
-//       const { latitude, longitude } = action.payload
-//       state.latitude = latitude
-//       state.longitude = longitude
-//       state.path.push({ latitude, longitude })
-//     },
-//   },
-// })
-
-// export const { setLocation } = locationSlice.actions
-// export default locationSlice.reducer
-
 import { createSlice } from "@reduxjs/toolkit"
 
 const locationSlice = createSlice({
   name: "location",
   initialState: {
-    latitude: null,
-    longitude: null,
-    path: [],
-    distance: 0,
-    steps: 0,
-    tracking: false, // Tracking state added
+    latitude: null, // 현재 경도 저장
+    longitude: null, // 현재 위도 저장
+    path: [], // 폴리곤을 저장하기 위한 path
+    distance: 0, // 산책 거리 저장
+    steps: 0, // 산책 걸음 수 저장
+    tracking: false, // 산책을 하고 있는지 확인 boolean
   },
   reducers: {
+    // 현재 위치 업데이트
     setLocation: (state, action) => {
       const { latitude, longitude } = action.payload
       state.latitude = latitude
@@ -41,18 +20,21 @@ const locationSlice = createSlice({
         state.path.push({ latitude, longitude })
       }
     },
+    // 현재 위치 초기화
     resetLocation: (state) => {
       state.latitude = null
       state.longitude = null
       state.path = []
       state.distance = 0
       state.steps = 0
-      state.tracking = false // Reset tracking state
+      state.tracking = false
     },
+    // 거리와 걸음걸이를 누적합
     updateDistanceAndSteps: (state, action) => {
       state.distance += action.payload.distance
       state.steps += action.payload.steps
     },
+    // 산책 상태를 업데이트
     setTrackingState: (state, action) => {
       state.tracking = action.payload
     },
